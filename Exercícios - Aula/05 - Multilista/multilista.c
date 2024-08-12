1996bd7cf09d7e08e26fe7e918cbbae484107f93
Gustavo Henrique <110679992+gustaaragao@users.noreply.github.com>
2024-08-12 14:53:05 -0300

    terminando multilista.c

--- Exercícios - Aula/05 - Multilista/multilista.c
+++ Exercícios - Aula/05 - Multilista/multilista.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Definição das estruturas
 typedef struct Aluno
 {
     uint16_t matricula;
@@ -32,6 +33,7 @@ typedef struct ListaDisciplinas
     uint32_t quantidade;
 } ListaDisciplinas;
 
+// Função para inicializar a lista de disciplinas
 ListaDisciplinas* inicializar_lista_disciplinas()
 {
     ListaDisciplinas *listaDisciplinas = malloc(sizeof(ListaDisciplinas));
@@ -48,34 +50,39 @@ ListaDisciplinas* inicializar_lista_disciplinas()
     return listaDisciplinas;
 }
 
-int criar_disciplina(char nome[50], uint8_t codigo, ListaDisciplinas *listaDisciplinas)
+// Função para criar uma nova disciplina
+int criar_disciplina(char *nome, uint8_t codigo, ListaDisciplinas *listaDisciplinas)
 {
-    // TODO: FAZER VERIFICAO SE HÁ UMA DISCIPLINA COM ESSE CODIGO
+    // Verifica se já existe uma disciplina com esse código
+    Disciplina *atual = listaDisciplinas->inicio;
+    while (atual != NULL) {
+        if (atual->codigo == codigo) {
+            printf("ERRO: JA EXISTE UMA DISCIPLINA COM ESSE CODIGO.\n");
+            return 1;
+        }
+        atual = atual->proximo;
+    }
 
+    // Aloca memória para a nova disciplina
     Disciplina *novaDisciplina = malloc(sizeof(Disciplina));
-
     if (novaDisciplina == NULL) {
         printf("[ERRO - ALOCACAO DE MEMORIA]: NAO FOI POSSIVEL ALOCAR MEMORIA PARA A NOVA DISCIPLINA.\n");
         return 1;
     }
 
+    // Aloca e inicializa a lista de alunos
     ListaAlunos *novaListaAlunos = malloc(sizeof(ListaAlunos));
-
     if (novaListaAlunos == NULL) {
-        printf("[ERRO - ALOCACAO DE MEMORIA]: NAO FOI POSSIVEL ALOCAR MEMORIA PARA A NOVA LISTA DE ALUNOS PARA A NOVA DISCIPLINA.\n");
+        printf("[ERRO - ALOCACAO DE MEMORIA]: NAO FOI POSSIVEL ALOCAR MEMORIA PARA A NOVA LISTA DE ALUNOS.\n");
         free(novaDisciplina);
         return 1;
     }
-
-    // Inicializando os campos da novaListaAlunos
     novaListaAlunos->quantidade = 0;
     novaListaAlunos->inicio = NULL;
     novaListaAlunos->final = NULL;
 
-    // Inicializando os campos da novaDisciplina
-    novaDisciplina->codigo = codigo; 
-
-    // Alocando e copiando o nome
+    // Inicializa os campos da novaDisciplina
+    novaDisciplina->codigo = codigo;
     novaDisciplina->nome = malloc(strlen(nome) + 1);
     if (novaDisciplina->nome == NULL) {
         printf("[ERRO - ALOCACAO DE MEMORIA]: NAO FOI POSSIVEL ALOCAR MEMORIA PARA O NOME DA DISCIPLINA.\n");
@@ -89,31 +96,120 @@ int criar_disciplina(char nome[50], uint8_t codigo, ListaDisciplinas *listaDisci
     novaDisciplina->matriculados = novaListaAlunos;
     novaDisciplina->proximo = NULL;
 
+    // Adiciona a nova disciplina à lista
     if (listaDisciplinas->inicio == NULL) {
-        // A lista de disciplinas está vazia
         listaDisciplinas->inicio = novaDisciplina;
         listaDisciplinas->fim = novaDisciplina;
     } else {
         listaDisciplinas->fim->proximo = novaDisciplina;
         listaDisciplinas->fim = novaDisciplina;
     }
-    
+
     listaDisciplinas->quantidade++;
     return 0;
 }
 
-int matricular_aluno_na_disciplina(uint8_t codigoDisciplina, ListaDisciplinas *listaDisciplinas, char nomeAluno[50], uint16_t matricula)
+// Função para matricular um aluno em uma disciplina
+int matricular_aluno_na_disciplina(uint8_t codigoDisciplina, char *nomeAluno, uint16_t matricula, ListaDisciplinas *listaDisciplinas)
 {
+    Disciplina *disciplinaAlvo = listaDisciplinas->inicio;
 
-    
+    while (disciplinaAlvo != NULL) {
+        if (disciplinaAlvo->codigo == codigoDisciplina) {
+            // Aloca memória para o novo aluno
+            Aluno *novoAluno = malloc(sizeof(Aluno));
+            if (novoAluno == NULL) {
+                printf("[ERRO - ALOCACAO DE MEMORIA]: NAO FOI POSSIVEL ALOCAR MEMORIA PARA O NOVO ALUNO.\n");
+                return 1;
+            }
 
+            // Inicializa os campos do novoAluno
+            novoAluno->nome = malloc(strlen(nomeAluno) + 1);
+            if (novoAluno->nome == NULL) {
+                printf("[ERRO - ALOCACAO DE MEMORIA]: NAO FOI POSSIVEL ALOCAR MEMORIA PARA O NOME DO NOVO ALUNO.\n");
+                free(novoAluno);
+                return 1;
+            }
+            strcpy(novoAluno->nome, nomeAluno);
+            novoAluno->matricula = matricula;
+            novoAluno->proximo = NULL;
+
+            // Adiciona o novo aluno à lista de alunos da disciplina
+            if (disciplinaAlvo->matriculados->inicio == NULL) {
+                disciplinaAlvo->matriculados->inicio = novoAluno;
+                disciplinaAlvo->matriculados->final = novoAluno;
+            } else {
+                disciplinaAlvo->matriculados->final->proximo = novoAluno;
+                disciplinaAlvo->matriculados->final = novoAluno;
+            }
+
+            disciplinaAlvo->matriculados->quantidade++;
+            return 0;
+        }
+        disciplinaAlvo = disciplinaAlvo->proximo;
+    }
+
+    printf("ERRO: O CODIGO DA DISCIPLINA NAO CONSTA NO BANCO DE DADOS.\n");
+    return 1;
 }
 
-void excluir_aluno_da_disciplina(uint8_t codigoDisciplina, ListaDisciplinas *listaDisciplinas, uint16_t matricula)
+int excluir_aluno_da_disciplina(uint8_t codigoDisciplina, uint16_t matricula, ListaDisciplinas *listaDisciplinas)
 {
+    Disciplina *disciplinaAlvo = listaDisciplinas->inicio;
 
+    while (disciplinaAlvo != NULL) {
+        if (disciplinaAlvo->codigo == codigoDisciplina) {
+            
+            Aluno *alunoAtual = disciplinaAlvo->matriculados->inicio;
+            Aluno *alunoAnterior = NULL;
+
+            while (alunoAtual != NULL) {
+                if (alunoAtual->matricula == matricula) {
+                    // Encontramos o aluno a ser excluido
+                    if (alunoAnterior == NULL) {
+                        // Esse aluno é o primeiro da lista
+                        disciplinaAlvo->matriculados->inicio = alunoAtual->proximo;
+                        if (disciplinaAlvo->matriculados->inicio == NULL) {
+                            // A lista agora está vazia
+                            disciplinaAlvo->matriculados->final = NULL;
+                        }
+                    } else {
+                        // O aluno está no meio da lista ou no final
+                        alunoAnterior->proximo = alunoAtual->proximo;
+
+                        if (alunoAtual->proximo == NULL) {
+                            // O aluno está no final da lista
+                            disciplinaAlvo->matriculados->final = alunoAnterior;
+                        }
+                    }
+
+
+                    free(alunoAtual->nome);
+                    free(alunoAtual);            
+
+                    disciplinaAlvo->matriculados->quantidade--;
+
+                    printf("Aluno %d deletado com sucesso!\n", matricula);
+
+                    return 0;
+                }
+
+                alunoAtual = alunoAtual->proximo;
+            }
+
+            printf("ERRO: O ALUNO NAO ESTA MATRICULADO NA DISCIPLINA");
+
+            return 1;
+        }
+
+        disciplinaAlvo = disciplinaAlvo->proximo;
+    }
+
+    printf("ERRO: O CODIGO DA DISCIPLINA NAO CONSTA NO BANCO DE DADOS.\n");
+    return 1;
 }
 
+// Função para exibir a lista de disciplinas
 void exibir_lista_disciplinas(ListaDisciplinas *listaDisciplinas)
 {
     Disciplina *atual = listaDisciplinas->inicio;
@@ -123,35 +219,33 @@ void exibir_lista_disciplinas(ListaDisciplinas *listaDisciplinas)
         return;
     }
 
-    do
-    {
+    while (atual != NULL) {
         printf("Codigo: %d \t Nome: %s \t Matriculados: %d\n", atual->codigo, atual->nome, atual->matriculados->quantidade);
         atual = atual->proximo;
-    } while (atual != NULL);
+    }
 }
 
-void exibir_matriculados(ListaAlunos *ListaAlunos)
+// Função para exibir os alunos matriculados
+void exibir_matriculados(ListaAlunos *listaAlunos)
 {
-    Aluno *atual = ListaAlunos->inicio;
+    Aluno *atual = listaAlunos->inicio;
 
     if (atual == NULL) {
         printf("Nao ha alunos matriculados.\n");
         return;
     }
-    
-    do
-    {
-        if (atual->proximo == NULL) {
-            // Ultimo aluno da lista
-            printf("%s\n", atual->nome);
-        } else {
-            printf("%s, ", atual->nome);
-        }
 
+    while (atual != NULL) {
+        printf("%s", atual->nome);
+        if (atual->proximo != NULL) {
+            printf(", ");
+        }
         atual = atual->proximo;
-    } while (atual != NULL);
+    }
+    printf("\n");
 }
 
+// Função para exibir a lista de disciplinas com os alunos matriculados
 void exibir_lista_disciplinas_com_alunos(ListaDisciplinas *listaDisciplinas)
 {
     Disciplina *atual = listaDisciplinas->inicio;
@@ -161,13 +255,11 @@ void exibir_lista_disciplinas_com_alunos(ListaDisciplinas *listaDisciplinas)
         return;
     }
 
-    do
-    {
+    while (atual != NULL) {
         printf("Codigo: %d \t Nome: %s \t Alunos: ", atual->codigo, atual->nome);
         exibir_matriculados(atual->matriculados);
-
         atual = atual->proximo;
-    } while (atual != NULL);
+    }
 }
 
 int main()
@@ -177,6 +269,18 @@ int main()
     criar_disciplina("Estrutura de Dados", 100, listaDisciplinas);
     criar_disciplina("Programacao Orientada a Objetos", 101, listaDisciplinas);
     criar_disciplina("Linguagens Formais e Computabilidade", 102, listaDisciplinas);
+
+    matricular_aluno_na_disciplina(100, "Gustavo", 1, listaDisciplinas);
+    matricular_aluno_na_disciplina(100, "Henrique", 2, listaDisciplinas);
+    matricular_aluno_na_disciplina(100, "Aragao", 3, listaDisciplinas);
+
+    exibir_lista_disciplinas(listaDisciplinas);
+
+    printf("\n");
+
+    exibir_lista_disciplinas_com_alunos(listaDisciplinas);
+
+    excluir_aluno_da_disciplina(100, 1, listaDisciplinas);
 
     exibir_lista_disciplinas(listaDisciplinas);
 
